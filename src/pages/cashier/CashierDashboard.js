9c92bee113fb725d83d9bfa9cd9150553dcb6ae3
Sachitha Hirushan <shpsachitha@gmail.com>
2022-11-03 18:25:24 +0530

    cashier dashboard done

--- src/pages/cashier/CashierDashboard.js
+++ src/pages/cashier/CashierDashboard.js
@@ -125,26 +125,43 @@ export default function CashierDashboard() {
             }
           </Grid>
         </Grid>
+        {/* <Divider sx={{ bgcolor: '#B5986D' }} /> */}
+        <div>
+          <Grid container padding={3} columns={{ xs: 12, sm: 12, md: 12 }}>
+            <Grid item xs={3} sm={3} md={3}>
+              Order No
+            </Grid>
+            <Grid item xs={3} sm={3} md={3}>
+              Customer Name
+            </Grid>
+            <Grid item xs={3} sm={3} md={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              Amount
+            </Grid>
+            <Grid item xs={3} sm={3} md={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              Order Status
+            </Grid>
+          </Grid>
+        </div>
+
+        {/* Sample data start */}
         <Divider sx={{ bgcolor: '#B5986D' }} />
         <div>
           <Grid container padding={3} columns={{ xs: 12, sm: 12, md: 12 }}>
             <Grid item xs={3} sm={3} md={3}>
-              Generate Report For Date
+              1004
             </Grid>
             <Grid item xs={3} sm={3} md={3}>
-              <TextField
-                style={{ color: 'white' }}
-                name="date"
-                type="date"
-                value={forDate}
-                onChange={(e) => setForDate(e.target.value)}
-              />
+              sachitha hirushan
             </Grid>
             <Grid item xs={3} sm={3} md={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Button style={{ backgroundColor: '#175A00', color: '#FFF', margin: '5px' }}>Print</Button>
+              750
+            </Grid>
+            <Grid item xs={3} sm={3} md={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              Accepted
             </Grid>
           </Grid>
         </div>
+        {/* Sample data end */}
       </Container>
     </>
   );
