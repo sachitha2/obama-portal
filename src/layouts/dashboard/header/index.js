de031b5f30d89669a7224d9c9a7ae936867447ea
Sachitha Hirushan <shpsachitha@gmail.com>
2022-11-04 11:14:39 +0530

    wip

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -48,7 +48,7 @@ export default function Header({ onOpenNav }) {
     // KITCHEN_MANAGER ,ADMIN, CUSTOMER, CASHIER,STOCK_KEEPER
     const role = getCookie('role');
     setUserRole(role);
-    setUserRole('CUSTOMER')
+    setUserRole('ADMIN')
   }, [])
 
   const navigate = useNavigate();
